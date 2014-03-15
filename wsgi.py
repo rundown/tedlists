8ba8795886036c4f8accf19981e9cf5b94ee2267
Gaurav Joseph <gaurav.joseph@linuxmail.org>
2014-03-15 18:43:58 +0530

    First Test

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+from apiclient.discovery import build
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
